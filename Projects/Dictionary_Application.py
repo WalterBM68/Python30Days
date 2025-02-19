19578aa6e877ac620ecf63bb4a6ea115f1940e40
Walter Manjanja <wamanjajhb024@student.wethinkcode.co.za>
2025-02-19 12:48:24 +0200

    fixed the sentence

--- Projects/Dictionary_Application.py
+++ Projects/Dictionary_Application.py
@@ -4,4 +4,4 @@
 def words_with_meaning():
     word = input("Enter the word: ").capitalize()
     meaning = input("enter the meaning: ").capitalize()
-    example = input("use it in a sentence: ")
+    example = input("use it in a sentence: ").capitalize()

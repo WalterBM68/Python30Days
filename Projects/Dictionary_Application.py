00b50a1f13a3f74d4d22270167cf2b7abc973c3d
Walter Manjanja <wamanjajhb024@student.wethinkcode.co.za>
2025-02-19 12:47:08 +0200

    the structure is created

--- Projects/Dictionary_Application.py
+++ Projects/Dictionary_Application.py
@@ -1,1 +1,7 @@
 """The dictionary project"""
+
+#enter words and their explaination
+def words_with_meaning():
+    word = input("Enter the word: ").capitalize()
+    meaning = input("enter the meaning: ").capitalize()
+    example = input("use it in a sentence: ")

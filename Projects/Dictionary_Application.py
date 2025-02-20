5fd66afe77145fc59d1f8f2af0d4333fa2adfae1
Walter Manjanja <wamanjajhb024@student.wethinkcode.co.za>
2025-02-20 20:38:07 +0200

    few changes have been made

--- Projects/Dictionary_Application.py
+++ Projects/Dictionary_Application.py
@@ -2,6 +2,7 @@
 
 #enter words and their explaination
 def words_with_meaning():
+    """allow the user to enter his/her choice of words with the meaning"""
     word = input("Enter the word: ").capitalize()
     meaning = input("enter the meaning: ").capitalize()
-    example = input("use it in a sentence: ").capitalize()
+    example = input("use it in a sentence: ").capitalize()  

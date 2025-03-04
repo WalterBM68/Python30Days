c3c73b7b349fe1026573c11848fbc6ba01c2f2b6
Walter Manjanja <wamanjajhb024@student.wethinkcode.co.za>
2025-03-04 22:29:54 +0200

    change

--- Projects/Dictionary_Application.py
+++ Projects/Dictionary_Application.py
@@ -6,3 +6,6 @@ def words_with_meaning():
     word = input("Enter the word: ").capitalize()
     meaning = input("enter the meaning: ").capitalize()
     example = input("use it in a sentence: ").capitalize()  
+
+
+
